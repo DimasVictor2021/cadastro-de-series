cfa44fffca6eb01d4dc420bc801a43e43e000bbe
DimasVictor2021 <dimasvictor2006@hotmail.com>
2021-04-02 22:44:54 -0300

    Meu segungo commit

---
+++ Cadastro de Série/Classes/EntidadeBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadastro_de_Série.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
